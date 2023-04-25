fc35eb5af0a7f470ca441bd0b4ccb979a5dd0c83
Vinay Singirikonda <kingking148414@gmail.com>
2023-04-25 23:44:20 +0530

    Fixed bugs and updated

--- script.js
+++ script.js
@@ -27,13 +27,16 @@ const navLinks = document.querySelectorAll('.navbar a');
 
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.bottom <= windowHeight * 1.6 &&
+        rect.right <= windowWidth
     );
 }
+
 
 window.addEventListener('scroll', () => {
     sections.forEach((section, index) => {
